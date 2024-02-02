dc47d809fb9adc62c28ac618ac487e52544c19a5
Kemitha-Fernando <kemithafernan@gmail.com>
2024-02-02 22:32:10 +0530

    update

--- Python/Kemitha/listActivity.py
+++ Python/Kemitha/listActivity.py
@@ -3,5 +3,6 @@ marks=[]
 for i in range(len(sub)):
     userinput=int(input("enter marks for "+sub[i]+": "))
     marks.append(userinput)
+print()
 for i in range(len(sub)):
     print("Your marks for",sub[i],"is",marks[i])
