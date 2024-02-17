209ee67afeb0d2f349d2a3fe922f43faa74ada99
Kemitha-Fernando <kemithafernan@gmail.com>
2024-02-17 21:30:59 +0530

    new

---
+++ Python/Kemitha/Cal.py
@@ -0,0 +1,17 @@
+print("addition  , multification for * , subtraction for - , divition for /")
+w=input("enter ur choice")
+
+a= int(input("ur first num"))
+b= int(input("ur second num"))
+def add(a,b):
+    x=a+b
+    print(x)
+def sub(a,b):
+    x=a-b
+    print(x)
+def mul(a,b):
+    x=a*b
+    print(x)
+def divi(a,b):
+    x=a/b
+    print(x)
---
+++ Python/Kemitha/TupleTsts.py
@@ -0,0 +1,14 @@
+st1=(1,2,3,4)
+st2=(10,20,30,40)
+
+print(st1)
+print(st2)
+
+new=st2+st1
+print("New Tuple is : ",new)
+
+del st2
+
+print(st1)
+#print(st2)
+print(st1*2)
