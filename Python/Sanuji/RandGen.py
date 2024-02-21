9002f74b003a0043de0605b7fc6d20ef2b1b0d6b
Sanuji Dilrukshi <sanuji.dilrukshi99.com>
2024-02-21 22:00:34 +0530

    Function

---
+++ Python/Sanuji/RandGen.py
@@ -0,0 +1,5 @@
+import random
+
+# Generate a random integer between 1 and 100
+random_number = random.randint(1, 100)
+print("Random number:", random_number)
---
+++ Python/Sanuji/qr/fun.py
@@ -0,0 +1,7 @@
+def add(a,b=5):
+    print(a+b)
+
+
+add(10)
+print("----")
+add(10,20)
